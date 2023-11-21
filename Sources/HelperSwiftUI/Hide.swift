d853f24ec36e319edf05a91f63786d11526df0cc
Pavel Koyushev <pkoyushev@gmail.com>
2023-11-21 21:41:14 +0300

    hide ext

--- Sources/HelperSwiftUI/Hide.swift
+++ Sources/HelperSwiftUI/Hide.swift
@@ -12,4 +12,21 @@ public extension View {
     func hide(_ condition: Bool) -> some View {
         opacity(condition ? 0 : 1)
     }
+    
+    @ViewBuilder
+    func hide(_ condition: VisibleState) -> some View {
+        switch condition {
+        case .removed:
+            EmptyView()
+        default:
+            opacity(condition == .visible ? 1 : 0)
+        }
+    }
+}
+
+public enum VisibleState {
+    
+    case visible
+    case invisible
+    case removed
 }
